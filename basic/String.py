9e17fd0afdf8bb332875135bf0f43de23267667d
hzy9738 <hzy9738@126.com>
2018-08-03 18:46:44 +0800

    basic_1

---
+++ basic/String.py
@@ -0,0 +1,12 @@
+print('hello world')
+print("hello world")
+print("hello \" s world")
+print("hello' s world")
+print('''
+    你妹啊
+    cnm
+''')
+print("""
+    你妹的
+    啊啊啊
+""")
---
+++ basic/number.py
@@ -0,0 +1,22 @@
+# 类型
+print(type(True))
+print(type(False))
+
+# 转换
+print(int(True))
+print(int(False))
+# True
+print(bool(1))
+print(bool(123.1))
+print(bool(-1.2))
+print(bool([1,2,3]))
+print(bool({1,2,3}))
+# False
+print(bool([]))
+print(bool({}))
+print(bool(0))
+print(bool(None))
+
+
+# 复数
+print(36j)
