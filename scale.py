22b2d3e10ced8e3dbeae73d8f4f2fac432d8e384
hzy9738 <hzy9738@126.com>
2018-08-03 11:31:30 +0800

    scale

---
+++ scale.py
@@ -0,0 +1,26 @@
+# 进制
+print(0b11)      # 二进制
+
+print(0o10)      # 八进制
+
+print(0x10)      # 十六进制
+
+#转换成二进制
+print(bin(0b11))
+print(bin(0o10))
+print(bin(0x10))
+
+#转换成八进制
+print(hex(0b11))
+print(hex(0o10))
+print(hex(0x10))
+
+#转换成十进制
+print(int(0b11))
+print(int(0o10))
+print(int(0x10))
+
+#转换成十六进制
+print(oct(0b11))
+print(oct(0o10))
+print(oct(0x10))
