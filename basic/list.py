c7c267aaa496faacb854224e1b5badec81297f67
hzy9738 <hzy9738@126.com>
2018-08-13 20:50:15 +0800

    'set'

---
+++ basic/list.py
@@ -0,0 +1,12 @@
+print(3 in [1,2,3,4,5])
+print(3 not in [1,2,3,4,5])
+
+print(len([1,2,3,4,5]))
+print(len('hello world'))
+
+print(max([1,2,3,4,5]))
+print(max('hello world'))
+
+print(min([1,2,3,4,5]))
+print(min('helloworld'))
+print(min('hello world'))
--- basic/transform.py
+++ basic/transform.py
@@ -3,3 +3,19 @@ print('hello \n world')
 print('hello \\n world')
 
 print(r'hello \\n world')
+
+
+
+print('hello ' + 'world')
+
+print('hello world'[0])
+print('hello world'[1])
+print('hello world'[-1])
+print('hello world'[0:4])
+print('hello world'[0:5])
+print('hello world'[0:-1])
+
+
+print('Don t know what to look for? Want more options? Switch to the full version'[4:])
+print('Don t know what to look for? Want more options? Switch to the full version'[:-7])
+print('Don t know what to look for? Want more options? Switch to the full version'[-7:])
